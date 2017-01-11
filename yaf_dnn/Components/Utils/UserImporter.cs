17fc8411193c1dbf5e6a773034a6191a202a5f3a
w8tcha <thewatcher@watchersnet.de>
2017-01-11 13:27:39 +0100

    now when import user in yaf the last login date is used for joined and lastvisit

--- yaf_dnn/Components/Utils/UserImporter.cs
+++ yaf_dnn/Components/Utils/UserImporter.cs
@@ -3,7 +3,7 @@
  * Copyright (C) 2006-2013 Jaben Cargman
  * Copyright (C) 2014-2016 Ingo Herbote
  * http://www.yetanotherforum.net/
- * 
+ *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -112,7 +112,7 @@ namespace YAF.DotNetNuke.Components.Utils
                     {
                         dnnUser.IsApproved = true;
                         Membership.UpdateUser(dnnUser);
-                    } 
+                    }
 
                     var yafUserId = LegacyDb.user_get(boardId, dnnUser.ProviderUserKey);
 
@@ -219,7 +219,7 @@ namespace YAF.DotNetNuke.Components.Utils
             if (dnnUserInfo.Profile.Website.IsSet())
             {
                 userProfile.Homepage = dnnUserInfo.Profile.Website;
-            } 
+            }
 
             userProfile.Save();
 
@@ -241,7 +241,8 @@ namespace YAF.DotNetNuke.Components.Utils
                 null,
                 null,
                 null,
-                null);
+                null,
+                dnnUserInfo.Membership.LastLoginDate.ToUniversalTime());
 
             var autoWatchTopicsEnabled =
                 boardSettings.DefaultNotificationSetting.Equals(UserNotificationSetting.TopicsIPostToOrSubscribeTo);
