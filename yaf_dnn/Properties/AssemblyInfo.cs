8f1deb5c343b12e8c37e428e72a1b3e1ce3e2185
w8tcha <thewatcher@watchersnet.de>
2017-09-28 19:23:16 +0200

    fixed issue with specific version

--- yaf_dnn/Properties/AssemblyInfo.cs
+++ yaf_dnn/Properties/AssemblyInfo.cs
@@ -40,4 +40,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("3d5900ae-111a-45be-96b3-d9e4606ca793")]
-
