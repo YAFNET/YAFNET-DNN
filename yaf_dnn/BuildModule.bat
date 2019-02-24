ca0ae26063ce2b8d3a37faa8ad7d5f15960caa26
w8tcha <thewatcher@watchersnet.de>
2019-02-24 08:15:30 +0100

    Update BuildModule.bat

--- yaf_dnn/BuildModule.bat
+++ yaf_dnn/BuildModule.bat
@@ -1,7 +1,8 @@
 @SET FrameworkSDKDir=
 @SET PATH=%FrameworkDir%;%FrameworkSDKDir%;%PATH%
 @SET LANGDIR=EN
+@SET MSBUILDPATH="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MsBuild.exe"
 
 .nuget\nuget.exe restore YAF.DNN.Module.sln
 
-"C:\Program Files (x86)\MSBuild\14.0\Bin\MsBuild.exe" YAF.DNN.Module.sln /p:Configuration=Release /t:Clean;Build /p:WarningLevel=0;CreateDnnPackages=true /flp1:logfile=errors.txt;errorsonly %1 %2 %3 %4 %5 %6 %7 %8 %9 
+%MSBUILDPATH% YAF.DNN.Module.sln /p:Configuration=Release /t:Clean;Build /p:WarningLevel=0;CreateDnnPackages=true /flp1:logfile=errors.txt;errorsonly %1 %2 %3 %4 %5 %6 %7 %8 %9 
